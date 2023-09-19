7f87bbd0d1c9a4df65cbea90b25408247b1bf62e
Avicena112 <145442222+Avicena112@users.noreply.github.com>
2023-09-19 20:37:33 +0700

    Add files via upload
    Latihan Hello World Avicena

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main (String[] arg) {
+        System.out.println (" Avicena :Hallo kaka");
+        System.out.println ("Gary : Yoi");
+        System.out.println("Avicena : lagi dimana ?");
+        System.out.println("Gary: Di Gondangdia");
+        System.out.print ("Jakarta \n");
+        System.out.print (" Avicena: Ok");
+    }
+}
